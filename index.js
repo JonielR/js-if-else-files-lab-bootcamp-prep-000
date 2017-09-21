44cfe5d195ec42b1d2bba75c6d30e0942370c2f0
Joniel Rocio <private_gboy@hotmail.com>
2017-09-21 22:37:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var name = "Susan"
 if (name === "Susan") {
   console.log("The name is correct")
+} else {
+  console.log("Joe")
 }
 var height = "70"
 if (height === "70") {
-  console.log("The is heigh is correct")
+  console.log("The height is correct")
+} else {
+  console.log("74")
 }
 
 // Don't worry about this
